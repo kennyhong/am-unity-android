524801f9a1a12e73e781aae89665d3893447c8b2
Kenny Hong <k.hongjiahan@gmail.com>
2020-01-23 13:06:18 -0600

    Example of receiver from Android Broadcast

---
+++ Assets/Scripts/Objects/EpsonSensorData.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EpsonSensorData
+{
+    public Acceleration Acceleration;
+    public LinearAcceleration LinearAcceleration;
+    public RotationVector RotationVector;
+    public Gyroscope Gyroscope;
+}
+
+[System.Serializable]
+public class Acceleration
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+[System.Serializable]
+public class RotationVector
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+[System.Serializable]
+public class LinearAcceleration
+{
+    public float x;
+    public float y;
+    public float z;
+}
+
+[System.Serializable]
+public class Gyroscope
+{
+    public float x;
+    public float y;
+    public float z;
+}
---
+++ Assets/Scripts/ReceiveUpdate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ReceiveUpdate : MonoBehaviour
+{
+    private AndroidJavaClass jc;
+    private EpsonSensorData sensorData;
+    private float initialOrientationX;
+    private float initialOrientationY;
+    private float initialOrientationZ;
+
+    //public TextMesh textbox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       jc = new AndroidJavaClass("com.example.myreceiver.MyReceiver");
+       jc.CallStatic("createInstance");
+        string javaMessage = jc.GetStatic<string>("text");
+        if (javaMessage != null)
+       {
+            sensorData = JsonUtility.FromJson<EpsonSensorData>(javaMessage);
+
+            //textbox.text = "Gyroscope: x = " + sensorData.Gyroscope.x + ",y = " + sensorData.Gyroscope.y + ", z = " + sensorData.Gyroscope.z;
+       }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        string javaMessage = jc.GetStatic<string>("text");
+        if (javaMessage != null)
+        {
+            sensorData = JsonUtility.FromJson<EpsonSensorData>(javaMessage);
+            transform.Rotate(-sensorData.Gyroscope.x, -sensorData.Gyroscope.y, 0);
+            //textbox.text = "Gyroscope: x = " + sensorData.Gyroscope.x + ",y = " + sensorData.Gyroscope.y + ", z = " + sensorData.Gyroscope.z;
+        }
+    }
+}
+
